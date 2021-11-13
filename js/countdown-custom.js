7028c02b4cc1ecf0cc84a87d33c4b26a899f966d
Mukhammadyusuf Abdurakhimov <icoderx@yandex.ru>
2021-11-14 03:36:37 +0500

    color updated

--- js/countdown-custom.js
+++ js/countdown-custom.js
@@ -1,6 +1,5 @@
-jQuery(document).ready(function() {
-        $(function () {
-            $('#defaultCountdown').countdown({until: new Date(2021, 03, 20, 8)}); // year, month, date, hour
-        });
-});		
-
+jQuery(document).ready(function () {
+  $(function () {
+    $("#defaultCountdown").countdown({ until: new Date(2021, 11, 11, 14) }); // year, month, date, hour
+  });
+});
